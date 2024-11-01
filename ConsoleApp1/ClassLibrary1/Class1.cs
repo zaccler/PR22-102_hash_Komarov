6149333ee3a10c6b1bda8061ae43737f1d0adc41
zaccler <vladkomarov05@mail.ru>
2024-11-01 22:03:36 +0700

    start

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] sourceBytePassw = Encoding.UTF8.GetBytes(password);
+                byte[] hashSourceBytePassw = sha256.ComputeHash(sourceBytePassw);
+                string hashPassw = BitConverter.ToString(hashSourceBytePassw).Replace("-", string.Empty);
+                return hashPassw;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ClassLibrary1/Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class Helper
+    {
+        private static construction_organizationEntities1 _context;
+
+        public static construction_organizationEntities1 GetContext()
+        {
+            // Проверка, установлено ли подключение; если нет, создается новое подключение
+            if (_context == null)
+            {
+                _context = new construction_organizationEntities1(); // Создание нового подключения к БД
+            }
+            return _context; // Возвращение текущего подключения
+        }
+
+        public void CreateUser(Logins user)
+        {
+            _context.Users.Add(user); // Добавление записи нового пользователя в таблицу Users
+            _context.SaveChanges(); // Сохранение изменений в БД
+        }
+
+    }
+}
